4f59d52673e1daba80e7376570276032b8dc24fd
dyna-sawada <szb03072@edu.osakafu-u.ac.jp>
2021-11-12 11:30:50 +0900

    cal ratio (exact matching).

--- src/preprocess/count_fb_agreement.py
+++ src/preprocess/count_fb_agreement.py
@@ -20,11 +20,13 @@ df_lo_group = df_data.groupby('LO_ID')
 
 flag_list = [0] * len(df_data.index)
 
+
+count_all = 0
 for lo_id in lo_ids:
 
     df_l_g = df_lo_group.get_group(lo_id)
     
-    duplicated_df = df_l_g[df_l_g.duplicated(subset=['TEMPLATE_ID'], keep=False)]
+    duplicated_df = df_l_g[df_l_g.duplicated(subset=['FB_REFERENCE_ID'], keep=False)]
 
     inds = duplicated_df.index.values
 
@@ -36,25 +38,29 @@ for lo_id in lo_ids:
         j = i + 1
         
         while j != len(inds):
-            temp_id_1 = duplicated_df.at[inds[i], 'TEMPLATE_ID']
-            temp_id_2 = duplicated_df.at[inds[j], 'TEMPLATE_ID']
-            if temp_id_1 == temp_id_2:
-                ref_id_1 = duplicated_df.at[inds[i], 'FB_REFERENCE_ID']
-                ref_id_2 = duplicated_df.at[inds[j], 'FB_REFERENCE_ID']
+            ref_id_1 = duplicated_df.at[inds[i], 'FB_REFERENCE_ID']
+            ref_id_2 = duplicated_df.at[inds[j], 'FB_REFERENCE_ID']
+            if ref_id_1 == ref_id_2:
+                temp_id_1 = duplicated_df.at[inds[i], 'TEMPLATE_ID']
+                temp_id_2 = duplicated_df.at[inds[j], 'TEMPLATE_ID']
+        
                 print(
-                    "ind1:{}\tind2:{}\ttemp:{}\tref1:{}\tref2:{}".format(
-                        i, j, temp_id_1, ref_id_1, ref_id_2
+                    "ind1:{}\tind2:{}\tref:{}\ttemp1:{}\ttemp2:{}".format(
+                        i, j, ref_id_1, temp_id_1, temp_id_2
                         )
                     )
 
-                if ref_id_1 in ref_id_2 or ref_id_2 in ref_id_1:
+                if temp_id_1 == temp_id_2:
                     flag_list[inds[i]] = 1
                     flag_list[inds[j]] = 1
 
             j += 1
 
+    count_all += len(inds)
+
+
 print(flag_list)
-print(sum(flag_list), sum(flag_list)/len(flag_list)*100)
+print(sum(flag_list), count_all, sum(flag_list)/count_all * 100)
 
 
 
