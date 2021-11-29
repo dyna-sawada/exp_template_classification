acf956bb14ffeca9b42dd3aff326d01d801027e4
dyna-sawada <szb03072@edu.osakafu-u.ac.jp>
2021-11-29 19:29:15 +0900

    compute label-based metrics.

--- src/baseline_model.py
+++ src/baseline_model.py
@@ -12,11 +12,37 @@ from sklearn.metrics import recall_score, precision_score, f1_score
 #from sklearn.metrics import classification_report
 from sklearn.metrics import coverage_error, roc_auc_score, average_precision_score, label_ranking_loss
 
-from model import FocalLoss
-
 
 
 def one_error(y_true, y_pred):
+    one_error_list = []
+    for y_p, y_t in zip(y_pred, y_true):
+        sorted_idx = np.argsort(-y_p)
+        y_p = y_p[sorted_idx]
+        y_t = y_t[sorted_idx]
+
+        if np.sum(y_t) == 0:
+            score = np.nan
+        else:
+            count = 0
+            for p, t in zip(y_p, y_t):
+                if t == 1:
+                    break
+                else:
+                    count += 1
+            score = count / len(y_t)
+
+        one_error_list.append(score)
+
+    assert len(one_error_list) == 25
+
+    one_error_scores = np.array(one_error_list)
+    one_error_average = np.nanmean(one_error_scores)
+
+    return one_error_average, one_error_scores
+
+
+def _one_error(y_true, y_pred):
     count = 0
     for y_p, y_t in zip(y_pred, y_true):
         top_cls = np.argmax(y_p)
@@ -30,13 +56,12 @@ def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x))
 
 
+
 def main(args):
     temp_id_gold_dir = './work/temp_id_gold.json'
     temp_id_gold = json.load(open(temp_id_gold_dir))
 
-    params_dict = json.load(open('./out_test/params.json'))
-    #loss_fn = FocalLoss(gamma=2, alpha=None)
-    loss_fn = nn.BCELoss()
+    params_dict = json.load(open('./out_test/params.json'))    
 
     y_true = [] 
     for _lo_id, lo_id_dict in temp_id_gold.items():
@@ -47,8 +72,6 @@ def main(args):
             y_true.append(y_t)
     y_true = np.array(y_true)
     
-    #n_lo_id = len(y_true)
-    #n_temp_id = len(y_true[0])
     np_y_true = np.array(y_true)
     n_each_temp_id = np.sum(np_y_true, axis=0)
 
@@ -94,38 +117,39 @@ def main(args):
             y_pred_i = np.tile(y_pred_i, (n_batch, 1))
 
 
-        loss = loss_fn(
-                torch.from_numpy(y_pred_i.astype(np.float32)),
-                torch.from_numpy(y_true_i.astype(np.float32))
-                )
-        coverage = coverage_error(y_true_i, y_pred_i)
-
         #mAP_m = average_precision_score(y_true_i, y_pred_i, average='micro')
         mAP_w = average_precision_score(y_true_i, y_pred_i, average='weighted')
         #mAP_s = average_precision_score(y_true_i, y_pred_i, average='samples')
 
         roc_auc = roc_auc_score(y_true_i, y_pred_i, average='micro')
 
-        rank_loss = label_ranking_loss(y_true_i, y_pred_i)
-        one_err = one_error(y_true_i, y_pred_i)
+        #y_pred_01 = np.where(y_pred_i >= 0.5, 1, 0)
+        #micro_f1 = f1_score(y_pred_01, y_true_i, average='micro', zero_division=0)
+        #macro_f1 = f1_score(y_pred_01, y_true_i, average='macro', zero_division=0)
 
-        y_pred_01 = np.where(y_pred_i >= 0.5, 1, 0)
-        micro_f1 = f1_score(y_pred_01, y_true_i, average='micro', zero_division=0)
-        macro_f1 = f1_score(y_pred_01, y_true_i, average='macro', zero_division=0)
+
+        ## compute label-based metrics.
+        y_pred_i = y_pred_i.T
+        y_true_i = y_true_i.T
+
+        coverage = coverage_error(y_true_i, y_pred_i)
+
+        rank_loss = label_ranking_loss(y_true_i, y_pred_i)
+        one_err_average, one_err_scores = one_error(y_true_i, y_pred_i)
 
         print(
-            'Iter: {}\tLoss: {:.3f}\t \
+            'Iter: {}\t\
             OneError: {:.3f}\tCoverageLoss: {:.3f}\t \
             RankingLoss: {:.3f}\tmAP weighted: {:.3f}\t \
             ROC AUC micro: {:.3f}'.format(
-                iter_i, loss, one_err, coverage, rank_loss, mAP_w, roc_auc
+                iter_i, one_err_average, coverage, rank_loss, mAP_w, roc_auc
                 )
             )
-        print(
-            'F1 Micro: {:.3f}\tF1 Macro: {:.3f}'.format(
-                micro_f1, macro_f1
-                )
-            )
+        #print(
+        #    'F1 Micro: {:.3f}\tF1 Macro: {:.3f}'.format(
+        #        micro_f1, macro_f1
+        #        )
+        #    )
 
         break
     
