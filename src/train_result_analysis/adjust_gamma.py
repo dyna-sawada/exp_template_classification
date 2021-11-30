c8447083e5f95b04e2847168a5484b52798fc2ee
dyna-sawada <szb03072@edu.osakafu-u.ac.jp>
2021-11-30 14:39:01 +0900

    add roc scores to determine gamma of focal loss's hyper parameter.

--- src/train_result_analysis/adjust_gamma.py
+++ src/train_result_analysis/adjust_gamma.py
@@ -1,152 +1,185 @@
 
+import os
+import sys
 import matplotlib.pyplot as plt
 import json
 import numpy as np
-from sklearn.metrics import average_precision_score, coverage_error, label_ranking_loss
+import argparse
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from evaluation_metrics import f1_threshold_score
+from evaluation_metrics import PR_AUC_score, ROC_AUC_score
+from evaluation_metrics import one_error_score, coverage_score, ranking_loss_score
 
 
-def one_error(y_true, y_pred):
-    one_error_list = []
-    for y_p, y_t in zip(y_pred, y_true):
-        sorted_idx = np.argsort(-y_p)
-        y_p = y_p[sorted_idx]
-        y_t = y_t[sorted_idx]
 
-        if np.sum(y_t) == 0:
-            score = np.nan
-        else:
-            count = 0
-            for p, t in zip(y_p, y_t):
-                if t == 1:
-                    break
-                else:
-                    count += 1
-            score = count / len(y_t)
-
-        one_error_list.append(score)
-
-
-    assert len(one_error_list) == 25
-
-    return np.array(one_error_list)
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '-md', '--mode', choices=['train', 'test'], default='train',
+    help='Train / Test'
+)
+parser.add_argument(
+    '-av', '--average', choices=[None, 'macro', 'weighted'], default='macro',
+    help='how to calculate PR/ROC AUC score.'
+)
+args = parser.parse_args()
 
 
 gamma_list = [0, 0.5, 1, 2, 5]
-##############
-# train data #
-##############
-losses, probs, mAPs = [], [], []
-for gamma in gamma_list:
-    tr_result_file = './out_test_gm={}/iter_0/train_log_fold_0.json'.format(gamma)
 
-    tr_result_data = json.load(open(tr_result_file))
+if args.mode == 'train':
+    ##############
+    # train data #
+    ##############
+    losses, probs= [], []
+    pr_averages, roc_averages = [], []
 
-    y_val_preds = tr_result_data['prediction']
-    y_val_trues = tr_result_data['gold']
+    for gamma in gamma_list:
+        tr_result_file = './out_test_gm={}/iter_0/train_log_fold_0.json'.format(gamma)
+
+        tr_result_data = json.load(open(tr_result_file))
+
+        y_val_preds = tr_result_data['prediction']
+        y_val_trues = tr_result_data['gold']
+        
+        #train_losses = result_data['train_losses']
+        valid_losses = tr_result_data['val_losses']
+        
+        #coverages = result_data['coverage_error']
+        #mAP = tr_result_data['mAP']
+        #roc_auc = result_data['ROC_AUC']
+
+
+        epoch = [e for e, _ in enumerate(valid_losses)]
+        
+        prob = []
+        pr_average_stock, roc_average_stock = [], []
+        for y_val_pred, y_val_true in zip(y_val_preds, y_val_trues):
+            np_y_val_pred = np.array(y_val_pred)
+            np_y_val_true = np.array(y_val_true)
+            gold_index = np.where(np_y_val_true==1)
+            p = np.average(np_y_val_pred[gold_index])
+            prob.append(p)
+
+            _pr_scores, pr_average = PR_AUC_score(np.array(y_val_true), np.array(y_val_pred), average=args.average)
+            _roc_scores, roc_average = ROC_AUC_score(np.array(y_val_true), np.array(y_val_pred), average=args.average)
+
+            pr_average_stock.append(pr_average)
+            roc_average_stock.append(roc_average)
+
+        probs.append(prob)
+        pr_averages.append(pr_average_stock)
+        roc_averages.append(roc_average_stock)
+        losses.append(valid_losses)
+        
+
+    #print(losses)
+    #print(probs)
+    #print(mAPs)
+    #print(pr_averages)
+    #print(roc_averages)
+
+
+    """
+    ## probability - loss
+    plt.plot(probs[0], losses[0], label='gamma={}'.format(gamma_list[0]))
+    plt.plot(probs[1], losses[1], label='gamma={}'.format(gamma_list[1]))
+    plt.plot(probs[2], losses[2], label='gamma={}'.format(gamma_list[2]))
+    plt.plot(probs[3], losses[3], label='gamma={}'.format(gamma_list[3]))
+    plt.plot(probs[4], losses[4], label='gamma={}'.format(gamma_list[4]))
+
+    plt.legend(bbox_to_anchor=(0, 1), loc='upper left', borderaxespad=1, fontsize=12)
+    plt.show()
+    """
+
+    ## mAP - loss
+    plt.plot(pr_averages[0], losses[0], label='gamma={}'.format(gamma_list[0]))
+    plt.plot(pr_averages[1], losses[1], label='gamma={}'.format(gamma_list[1]))
+    plt.plot(pr_averages[2], losses[2], label='gamma={}'.format(gamma_list[2]))
+    plt.plot(pr_averages[3], losses[3], label='gamma={}'.format(gamma_list[3]))
+    plt.plot(pr_averages[4], losses[4], label='gamma={}'.format(gamma_list[4]))
+
+    plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=1, fontsize=12)
+    plt.show()
+
+
+    ## epoch - mAP
+    plt.plot(epoch, pr_averages[0], label='gamma={}'.format(gamma_list[0]))
+    plt.plot(epoch, pr_averages[1], label='gamma={}'.format(gamma_list[1]))
+    plt.plot(epoch, pr_averages[2], label='gamma={}'.format(gamma_list[2]))
+    plt.plot(epoch, pr_averages[3], label='gamma={}'.format(gamma_list[3]))
+    plt.plot(epoch, pr_averages[4], label='gamma={}'.format(gamma_list[4]))
+
+    plt.legend(bbox_to_anchor=(1, 0), loc='lower right', borderaxespad=1, fontsize=10)
+    plt.show()
+
+    ## ROC - loss
+    plt.plot(roc_averages[0], losses[0], label='gamma={}'.format(gamma_list[0]))
+    plt.plot(roc_averages[1], losses[1], label='gamma={}'.format(gamma_list[1]))
+    plt.plot(roc_averages[2], losses[2], label='gamma={}'.format(gamma_list[2]))
+    plt.plot(roc_averages[3], losses[3], label='gamma={}'.format(gamma_list[3]))
+    plt.plot(roc_averages[4], losses[4], label='gamma={}'.format(gamma_list[4]))
+
+    plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=1, fontsize=12)
+    plt.show()
+
+
+    ## epoch - ROC
+    plt.plot(epoch, roc_averages[0], label='gamma={}'.format(gamma_list[0]))
+    plt.plot(epoch, roc_averages[1], label='gamma={}'.format(gamma_list[1]))
+    plt.plot(epoch, roc_averages[2], label='gamma={}'.format(gamma_list[2]))
+    plt.plot(epoch, roc_averages[3], label='gamma={}'.format(gamma_list[3]))
+    plt.plot(epoch, roc_averages[4], label='gamma={}'.format(gamma_list[4]))
+
+    plt.legend(bbox_to_anchor=(1, 0), loc='lower right', borderaxespad=1, fontsize=10)
+    plt.show()
+
+
+else:
+    ###############
+    # result data #
+    ###############
+    pr_ws = []
+    prses = []
+    one_errors = []
+    cov_losses = []
+    rank_losses = []
+    pr_averages, roc_averages = [], []
+
+    for gamma in gamma_list:
+        te_result_file = './out_test_gm={}/iter_0/results_fold_0.json'.format(gamma)
+        te_result_data = json.load(open(te_result_file))
+
+        preds = te_result_data['prediction']
+        golds = te_result_data['gold']
+
+        preds = np.array(preds)
+        golds = np.array(golds)
+
+        pr_scores, pr_average = PR_AUC_score(golds, preds, average=args.average)
+        roc_scores, roc_average = ROC_AUC_score(golds, preds, average=args.average)
+
+        #pr_ws.append(pr_w)
+        
+        pr_averages.append(pr_average)
+        roc_averages.append(roc_average)
+
+        
+        one_err_scores, one_err_average = one_error_score(golds, preds)
+        coverage = coverage_score(golds, preds)
+        rank_loss = ranking_loss_score(golds, preds)
+
+        one_errors.append(one_err_average)
+        cov_losses.append(coverage)
+        rank_losses.append(rank_loss)
+
+
+    print(pr_averages)
+    print(roc_averages)
+    print(np.argmax(pr_averages))
+    print(np.argmax(roc_averages))
+
+    #print(one_errors)
+    #print(cov_losses)
+    #print(rank_losses)
     
-    #train_losses = result_data['train_losses']
-    valid_losses = tr_result_data['val_losses']
-    
-    #coverages = result_data['coverage_error']
-    mAP = tr_result_data['mAP']
-    #roc_auc = result_data['ROC_AUC']
-
-    epoch = [e for e, _ in enumerate(valid_losses)]
-    
-    prob = []
-    for y_val_pred, y_val_true in zip(y_val_preds, y_val_trues):
-        np_y_val_pred = np.array(y_val_pred)
-        np_y_val_true = np.array(y_val_true)
-        gold_index = np.where(np_y_val_true==1)
-        p = np.average(np_y_val_pred[gold_index])
-        prob.append(p)
-
-    probs.append(prob)
-    losses.append(valid_losses)
-    mAPs.append(mAP)
-    
-
-#print(losses)
-#print(probs)
-#print(mAPs)
-
-
-"""
-## probability - loss
-plt.plot(probs[0], losses[0], label='gamma={}'.format(gamma_list[0]))
-plt.plot(probs[1], losses[1], label='gamma={}'.format(gamma_list[1]))
-plt.plot(probs[2], losses[2], label='gamma={}'.format(gamma_list[2]))
-plt.plot(probs[3], losses[3], label='gamma={}'.format(gamma_list[3]))
-plt.plot(probs[4], losses[4], label='gamma={}'.format(gamma_list[4]))
-
-plt.legend(bbox_to_anchor=(0, 1), loc='upper left', borderaxespad=1, fontsize=12)
-plt.show()
-"""
-
-## mAP - loss
-plt.plot(mAPs[0], losses[0], label='gamma={}'.format(gamma_list[0]))
-plt.plot(mAPs[1], losses[1], label='gamma={}'.format(gamma_list[1]))
-plt.plot(mAPs[2], losses[2], label='gamma={}'.format(gamma_list[2]))
-plt.plot(mAPs[3], losses[3], label='gamma={}'.format(gamma_list[3]))
-plt.plot(mAPs[4], losses[4], label='gamma={}'.format(gamma_list[4]))
-
-plt.legend(bbox_to_anchor=(1, 1), loc='upper right', borderaxespad=1, fontsize=12)
-plt.show()
-
-
-## epoch - mAP
-plt.plot(epoch, mAPs[0], label='gamma={}'.format(gamma_list[0]))
-plt.plot(epoch, mAPs[1], label='gamma={}'.format(gamma_list[1]))
-plt.plot(epoch, mAPs[2], label='gamma={}'.format(gamma_list[2]))
-plt.plot(epoch, mAPs[3], label='gamma={}'.format(gamma_list[3]))
-plt.plot(epoch, mAPs[4], label='gamma={}'.format(gamma_list[4]))
-
-plt.legend(bbox_to_anchor=(1, 0), loc='lower right', borderaxespad=1, fontsize=10)
-plt.show()
-
-
-
-###############
-# result data #
-###############
-pr_ws = []
-prses = []
-one_errors = []
-cov_losses = []
-rank_losses = []
-
-for gamma in gamma_list:
-    te_result_file = './out_test_gm={}/iter_0/results_fold_0.json'.format(gamma)
-    te_result_data = json.load(open(te_result_file))
-
-    preds = te_result_data['prediction']
-    golds = te_result_data['gold']
-    pr_w = te_result_data['mAP']
-    prs = average_precision_score(golds, preds, average=None)
-
-    pr_ws.append(pr_w)
-    prses.append(prs)
-
-    preds = np.array(preds).T
-    golds = np.array(golds).T
-    
-    one_err_list = one_error(golds, preds)
-    one_err = np.nanmean(one_err_list)
-    cov_loss = coverage_error(golds, preds)
-    rank_loss = label_ranking_loss(golds, preds)
-
-    one_errors.append(one_err)
-    cov_losses.append(cov_loss)
-    rank_losses.append(rank_loss)
-
-
-pr_ws = np.array(pr_ws)
-prses = np.array(prses)
-print(pr_ws)
-print(prses)
-print(np.argmax(pr_ws))
-print(np.argmax(prses, axis=0))
-
-#print(one_errors)
-#print(cov_losses)
-#print(rank_losses)
