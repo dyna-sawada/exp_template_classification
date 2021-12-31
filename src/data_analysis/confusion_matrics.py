314f1a681b1c26eeae47c79bf2a4741597ea8d79
dyna-sawada <szb03072@edu.osakafu-u.ac.jp>
2021-12-31 09:48:39 +0900

    count miss-match number between annotators

--- src/data_analysis/confusion_matrics.py
+++ src/data_analysis/confusion_matrics.py
@@ -19,11 +19,30 @@ temp_id_1 = df['temp_id_1'].to_list()
 temp_id_2 = df['temp_id_2'].to_list()
 
 
+valid_count = 0
+miss_count = 0
+for t1, t2 in zip(temp_id_1, temp_id_2):
+    if t1 == 0 or t2 == 0:
+        valid_count += 1
+    else:
+        if t1 != t2:
+            miss_count += 1
+
+print(
+    'N_all\t{}\nN_valid\t{}\nN_miss\t{}'.format(
+        len(temp_id_1),
+        valid_count,
+        miss_count
+    )
+)
+
+
 used_temp_id = set(temp_id_1) | set(temp_id_2)
 used_temp_id = sorted(list(used_temp_id))
 #print(used_temp_id)
 
 cm = confusion_matrix(temp_id_1, temp_id_2, labels=used_temp_id[1:])
+print('--- confusion matrics ---')
 print(cm)
 
 cm = pd.DataFrame(
